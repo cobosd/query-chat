231e310bea205b2b6e3629a34228cf9a64cb6f4f
cobosd <dnazarioc@gmail.com>
2023-05-04 14:43:31 -0400

    fix ui

--- app/chain.py
+++ app/chain.py
@@ -1,6 +1,7 @@
 from langchain.llms import OpenAI
 # from langchain.chains import ChatVectorDBChain
-from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT, QA_PROMPT)
+from langchain.chains.chat_vector_db.prompts import (
+    CONDENSE_QUESTION_PROMPT, QA_PROMPT)
 import os
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import LLMChain
@@ -9,12 +10,18 @@ from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_
 from langchain.chains import ConversationalRetrievalChain
 from langchain.agents import create_csv_agent, create_pandas_dataframe_agent
 from langchain.llms import OpenAI
+from dotenv import load_dotenv
+
 
 def load_agent(file):
-    """Contruct a chat chain to query vectorstore and return the answer. This already includes chat memory and streaming callback""" 
-    openai_api_key = os.environ['OPENAI_API_KEY']
-    
+    """Contruct a chat chain to query vectorstore and return the answer. This already includes chat memory and streaming callback"""
+
+    # Load environment variables from .env file
+    load_dotenv()
+    # Initialize OpenAI API key
+    api_key = os.getenv('APP_OPENAI_KEY')
     # agent = create_csv_agent(OpenAI(temperature=0), file, verbose=True)
-    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), file, verbose=True)
-    
+    agent = create_pandas_dataframe_agent(
+        OpenAI(temperature=0, openai_api_key=api_key), file, verbose=True)
+
     return agent
--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 
 # components
 # from utils import QdrantVectorstore
-from chain import load_agent
-from fileConversion import csv_types
+from app.chain import load_agent
+from app.fileConversion import csv_types
 import shutil
 
 
@@ -18,18 +18,19 @@ import os
 import csv
 
 
-
 # Initialization
 app = FastAPI()
-templates = Jinja2Templates(directory="templates/")
+templates = Jinja2Templates(directory="app/templates/")
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Initialize OpenAI API key
-openai.api_key =  os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv('APP_OPENAI_KEY')
 
-UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'uploads')
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
@@ -39,21 +40,25 @@ csv_file_path = []
 dataframe = pd.DataFrame()
 
 # API endpoints
+
+
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-    
+
 # Define endpoint for text completion API
+
+
 @app.post('/complete')
 async def complete_text(request: Request):
-    
+
     # Retrieve request data
-    data =  await request.json()
+    data = await request.json()
 
     dataframe = csv_types(data["filename"])
-    
+
     agent = load_agent(dataframe)
-    
+
     output = agent.run(data["question"])
 
     return {'answer': output}
@@ -62,41 +67,42 @@ async def complete_text(request: Request):
 @app.post("/uploadfile")
 async def upload_csv_file(file: UploadFile = File(...)):
     print('FILE:', file)
-    
+
     if not file.filename.endswith('.csv'):
         return {"error": "Invalid file format"}
 
     file_path = os.path.join(UPLOAD_FOLDER, file.filename)
-    
+
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     with open(file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
-        
+
         if len(csv_file_path) != 0:
-             csv_file_path.pop()
-             
+            csv_file_path.pop()
+
         csv_file_path.append(file_path)
-        
+
         for row in csv_reader:
             uploaded_data_list.append(row)
 
     return {"status": "File uploaded successfully"}
 
+
 @app.get("/deletefile")
 async def delete_csv_file():
-    
+
     try:
         if len(csv_file_path) != 0:
             os.remove(str(csv_file_path[0]))
-            csv_file_path.pop()           
+            csv_file_path.pop()
             print(f"{csv_file_path} successfully removed")
     except OSError as e:
         print(f"Error: {csv_file_path} - {e.strerror}")
-        
-    print("DELETE?:",csv_file_path)
-    
+
+    print("DELETE?:", csv_file_path)
+
     return {"status": "File deleted successfully"}
 
 
@@ -106,15 +112,13 @@ async def process_data():
     # Here you can access the uploaded_data list
     for row in dataframe:
         print(row)
-    
-    
-    
+
     return {"status": "Data processed successfully"}
 
 
 @app.get("/getdata")
 async def get_name():
-    
+
     if len(csv_file_path) == 0:
         print("No file uploaded")
         return {"message": None}
@@ -125,7 +129,7 @@ async def get_name():
 
 @app.get("/time")
 def get_current_time():
-  return {"time": datetime.now()}
+    return {"time": datetime.now()}
 
 
 if __name__ == "__main__":
