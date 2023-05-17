390223a360e3302af3a64224307f83eb9be6105a
cobosd <dnazarioc@gmail.com>
2023-05-17 16:01:02 -0400

    remove credentials

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ import pandas as pd
 import uvicorn
 import os
 import csv
+from twilio.rest import Client
 
 
 # Initialization
@@ -156,8 +157,8 @@ def notify():
     db_name = os.getenv('DB_NAME')
     db_user = os.getenv('DB_USER')
     db_password = os.getenv('DB_PASS')
-
-
+    courier_api = os.getenv('COURIER_API')
+    
     def connect_to_database():
         conn = psycopg2.connect(
             host=db_host,
@@ -168,17 +169,29 @@ def notify():
         return conn
 
 
+    # query = """
+    #     SELECT *
+    #     FROM company.matches
+    #     WHERE "date" = (SELECT date
+    #                     FROM company.matches
+    #                     WHERE "date" > CURRENT_TIMESTAMP 
+    #                     ORDER BY "date" ASC
+    #                     LIMIT 1);
+
+    #     """
     query = """
-        SELECT *
-        FROM company.matches
-        WHERE "date" = (SELECT date
-                        FROM company.matches
-                        WHERE "date" > CURRENT_TIMESTAMP 
-                        ORDER BY "date" ASC
-                        LIMIT 1);
-
+            SELECT *
+            FROM company.matches
+            WHERE date BETWEEN CURRENT_DATE AND (CURRENT_DATE + INTERVAL '7 days')
+            AND ((
+                (home_team = 'FIGUEIRENSE' OR away_team = 'FIGUEIRENSE')
+                AND category = 'U15'
+            )
+            OR (
+                (home_team = 'HERCÍLIO LUZ' OR away_team = 'HERCÍLIO LUZ')
+                AND category = 'U20'
+            ));
         """
-
     connection = connect_to_database()
 
     cursor = connection.cursor()
@@ -186,33 +199,61 @@ def notify():
 
     cursor.execute(query)
     res = cursor.fetchall()
-
-
-    print(res)
     
     connection.commit()
     cursor.close()
 
+    # print(res)
     if len(res) != 0:
-        payload = ""
+        payload = []
         
-        for row in res:
-            payload = str(row) + "<br>" + payload
+        for _, row in enumerate(res):    
+            payload.append({"game_nbr":  row[0], "date": datetime.strftime(row[1], '%d/%m/%Y'), "day": row[2], "time": str(row[3])[:-3], 
+                            "stadium": row[4], "city": row[5], "category": row[6], "home_team": row[7], 
+                            "home_score": row[8] or "", "away_team": row[9], "away_score": row[10] or ""})
             
-        client = Courier(auth_token="pk_prod_41XEG71BC44ZBWQ5813QAAF0EJKY")
+            
+        whatsapp_string = ''
+        for game in payload:
+            whatsapp_string += f"Categoria: {game['category']}\n"
+            whatsapp_string += f"Dia: {game['day']}\n"
+            whatsapp_string += f"Data: {game['date']}\n"
+            whatsapp_string += f"Casa: {game['home_team']}\n"
+            whatsapp_string += f"Visitante: {game['away_team']}\n"
+            whatsapp_string += f"Cidade: {game['city']}\n"
+            whatsapp_string += f"Estádio: {game['stadium']}\n\n"
+        
+        
 
+
+        # SEND WHATASPP MESSAGE
+        print(whatsapp_string) 
+                    
+        account_sid = 'ACd4ea3137ad4f530b1100ac23910275db'
+        auth_token = '5d16764dc172c338716830654516d451'
+        client = Client(account_sid, auth_token)
+                        
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body=whatsapp_string,
+        to='whatsapp:+13863012225'
+        )
+        whatsapp_string
+        client = Courier(auth_token=courier_api)
+        
+
+        # SEND EMAIL MESSAGE
         resp = client.send_message(
             message={
                 "to": {"email": receiver},
-                "template": "3GFKE3E9564M62GCSA5M3F8F499H",
+                "template": "8YNFZMTV8B4E4SMVCNBD8ZMNK6DJ",
                 "data": {
-                    "data": payload,
+                    "games": payload,
                 },
             }
         )
         
-        return {"message": "Notification sent"}
-
+        return {"message": "Notification sent", "response": resp}
 
 
 if __name__ == "__main__":
